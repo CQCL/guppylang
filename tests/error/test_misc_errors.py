152865d76386485f8fb64457e022bf3969fd492e
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2023-06-23 12:38:14 +0100

    Update tests/error/test_misc_errors.py
    Co-authored-by: Seyon Sivarajah <seyon.sivarajah@quantinuum.com>

--- tests/error/test_misc_errors.py
+++ tests/error/test_misc_errors.py
@@ -3,7 +3,7 @@ import pytest
 
 from tests.error.util import run_error_test
 
-path = pathlib.Path(__file__).parent.resolve() / "type_errors"
+path = pathlib.Path(__file__).parent.resolve() / "misc_errors"
 files = [x for x in path.iterdir() if x.is_file() if x.suffix == ".py" and x.name != "__init__.py"]
 
 
