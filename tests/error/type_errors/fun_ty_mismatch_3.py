97c6bf69f9e7e524fba5ecde8e2ec80fac2f8cd4
Mark Koch <mark.koch@quantinuum.com>
2023-08-01 15:00:04 +0100

    Add error tests

---
+++ tests/error/type_errors/fun_ty_mismatch_3.py
@@ -0,0 +1,11 @@
+from typing import Callable
+
+from tests.error.util import guppy
+
+
+@guppy
+def foo(x: int) -> int:
+    def bar(f: Callable[[int], bool]) -> bool:
+        return f(42)
+
+    return bar(foo)
