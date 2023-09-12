b1e0d4b0f3da32c8d68731cd4ccfaf88b8a6f5bc
Douglas Wilson <douglas.wilson@quantinuum.com>
2023-09-12 09:20:16 +0100

    Hack exporting test cases

--- tests/integration/test_call.py
+++ tests/integration/test_call.py
@@ -1,9 +1,9 @@
 from guppy.compiler import guppy, GuppyModule
-from tests.integration.util import validate
+from tests.integration.util import validate_module, validate
 
 
 def test_call():
-    module = GuppyModule("module")
+    module = GuppyModule("test_call")
 
     @module
     def foo() -> int:
@@ -13,11 +13,11 @@ def test_call():
     def bar() -> int:
         return foo()
 
-    validate(module.compile(exit_on_error=True))
+    validate_module(module, exit_on_error=True)
 
 
 def test_call_back(tmp_path):
-    module = GuppyModule("module")
+    module = GuppyModule("test_call_back")
 
     @module
     def foo(x: int) -> int:
@@ -27,7 +27,7 @@ def test_call_back(tmp_path):
     def bar(x: int) -> int:
         return x
 
-    validate(module.compile(exit_on_error=True))
+    validate_module(module, exit_on_error=True)
 
 
 def test_recursion():
@@ -35,11 +35,11 @@ def test_recursion():
     def main(x: int) -> int:
         return main(x)
 
-    validate(main)
+    validate(main, "test_recursion")
 
 
 def test_mutual_recursion():
-    module = GuppyModule("module")
+    module = GuppyModule("test_mutual_recursion")
 
     @module
     def foo(x: int) -> int:
@@ -49,7 +49,4 @@ def test_mutual_recursion():
     def bar(x: int) -> int:
         return foo(x)
 
-    validate(module.compile(exit_on_error=True))
-
-
-
+    validate_module(module, exit_on_error=True)
--- tests/integration/util.py
+++ tests/integration/util.py
@@ -1,12 +1,26 @@
 from typing import TypeVar
+import os
+from pathlib import Path
 
 import validator
 from guppy.hugr.hugr import Hugr
+from guppy.compiler import GuppyModule
 
+def export_hugr(hugr: bytes, name: str):
+    export_dir = os.environ.get("GUPPY_TEST_EXPORT_PATH");
+    if(export_dir):
+        export_path = Path(export_dir) / f"{name}.msgpack"
+        export_path.write_bytes(hugr)
 
-def validate(hugr: Hugr):
-    validator.validate(hugr.serialize())
+def validate(hugr: Hugr, name = None):
+    raw = hugr.serialize()
+    if name:
+        export_hugr(raw, name)
+    validator.validate(raw)
 
+def validate_module(module: GuppyModule, exit_on_error=True):
+    hugr: Hugr = module.compile(exit_on_error=exit_on_error)
+    return validate(hugr, module.name)
 
 class Decorator:
     def __matmul__(self, other):
