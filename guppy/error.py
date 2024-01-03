40bd94a31490ba9d2f926a8afa2d5ec1c5f1e4a2
Mark Koch <mark.koch@quantinuum.com>
2024-01-03 18:18:41 +0100

    Fix error column offset

--- guppy/error.py
+++ guppy/error.py
@@ -144,7 +144,7 @@ def format_source_location(
     source_lines = source.splitlines(keepends=True)
     end_col_offset = loc.end_col_offset
     if end_col_offset is None or (loc.end_lineno and loc.end_lineno > loc.lineno):
-        end_col_offset = len(source_lines[loc.lineno - 1])
+        end_col_offset = len(source_lines[loc.lineno - 1]) - 1
     s = "".join(source_lines[max(loc.lineno - num_lines, 0) : loc.lineno]).rstrip()
     s += "\n" + loc.col_offset * " " + (end_col_offset - loc.col_offset) * "^"
     s = textwrap.dedent(s).splitlines()
