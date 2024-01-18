67d1a7e79c5ff439595f1a0e13fd7f8e3d6fbc6f
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2024-01-18 17:25:33 +0000

    feat: Allow pytket circuits in py expressions (#115)
    Co-authored-by: Seyon Sivarajah <seyon.sivarajah@quantinuum.com>

--- guppylang/compiler/expr_compiler.py
+++ guppylang/compiler/expr_compiler.py
@@ -1,4 +1,5 @@
 import ast
+import json
 from collections.abc import Iterator
 from contextlib import contextmanager
 from typing import Any
@@ -314,4 +315,17 @@ def python_value_to_hugr(v: Any) -> val.Value | None:
                 return None
             return val.Tuple(vs=vs)
         case _:
+            # Pytket conversion is an optional feature
+            try:
+                import pytket
+
+                if isinstance(v, pytket.circuit.Circuit):
+                    from tket2.circuit import (  # type: ignore[import-untyped, import-not-found, unused-ignore]
+                        Tk2Circuit,
+                    )
+
+                    hugr = json.loads(Tk2Circuit(v).to_hugr_json())
+                    return val.FunctionVal(hugr=hugr)
+            except ImportError:
+                pass
             return None
--- tests/error/test_py_errors.py
+++ tests/error/test_py_errors.py
@@ -3,12 +3,21 @@ import pytest
 
 from tests.error.util import run_error_test
 
+
+try:
+    import tket2
+
+    tket2_installed = True
+except ImportError:
+    tket2_installed = False
+
+
 path = pathlib.Path(__file__).parent.resolve() / "py_errors"
 files = [
     x
     for x in path.iterdir()
     if x.is_file()
-    if x.suffix == ".py" and x.name != "__init__.py"
+    if x.suffix == ".py" and x.name not in ("__init__.py", "tket2_not_installed.py")
 ]
 
 # Turn paths into strings, otherwise pytest doesn't display the names
@@ -18,3 +27,9 @@ files = [str(f) for f in files]
 @pytest.mark.parametrize("file", files)
 def test_py_errors(file, capsys):
     run_error_test(file, capsys)
+
+
+@pytest.mark.skipif(tket2_installed, reason="tket2 is installed")
+def test_tket2_not_installed(capsys):
+    path = pathlib.Path(__file__).parent.resolve() / "py_errors" / "tket2_not_installed.py"
+    run_error_test(str(path), capsys)
