b68224bb6d16098186662f4caeeb1627033a3253
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2024-06-25 10:42:03 +0100

    chore: Improve formatting of error test files (#268)

--- tests/error/test_poly_errors.py
+++ tests/error/test_poly_errors.py
@@ -4,7 +4,11 @@ import pytest
 from tests.error.util import run_error_test
 
 path = pathlib.Path(__file__).parent.resolve() / "poly_errors"
-files = [x for x in path.iterdir() if x.is_file() if x.suffix == ".py" and x.name != "__init__.py"]
+files = [
+    x
+    for x in path.iterdir()
+    if x.is_file() and x.suffix == ".py" and x.name != "__init__.py"
+]
 
 # Turn paths into strings, otherwise pytest doesn't display the names
 files = [str(f) for f in files]
