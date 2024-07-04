aea8dab737010fd9fbce3a00738591b0e66c8432
Douglas Wilson <douglas.wilson@quantinuum.com>
2024-07-04 10:48:04 +0100

    add llvm nix

--- devenv.nix
+++ devenv.nix
@@ -1,31 +1,48 @@
-{ pkgs, lib, ... }:
-
-{
+{ pkgs, lib, config, ... }: let
+  cfg = config.llvm;
+  libllvm = pkgs."llvmPackages_${cfg.llvmVersion}".libllvm.dev;
+in {
   # for building optional tket2 dependency
   # see https://github.com/CQCL/tket2/blob/main/devenv.nix
-  packages = [
-    pkgs.just
-  ]
-  ++ lib.optionals pkgs.stdenv.isLinux [
-    pkgs.stdenv.cc.cc.lib
-  ]
-  ++ lib.optionals pkgs.stdenv.isDarwin (
-    with pkgs.darwin.apple_sdk; [
-      frameworks.CoreServices
-      frameworks.CoreFoundation
-    ]
-  );
-
-  languages.python = {
-    enable = true;
-    poetry = {
-      enable = true;
-      activate.enable = true;
+  options.llvm = {
+    llvmVersion = lib.mkOption {
+      type = lib.types.str;
+      default = "14";
     };
   };
+  config = {
+    packages = [
+      pkgs.just
+      libllvm
+      # these are required by libllvm
+      pkgs.libffi
+      pkgs.libxml2
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      pkgs.stdenv.cc.cc.lib
+    ]
+    ++ lib.optionals pkgs.stdenv.isDarwin (
+      with pkgs.darwin.apple_sdk; [
+        frameworks.CoreServices
+        frameworks.CoreFoundation
+      ]
+    );
 
-  languages.rust = {
-    enable = true;
-    channel = "stable";
+    env = {
+      "LLVM_SYS_${cfg.llvmVersion}0_PREFIX" = "${libllvm}";
+    };
+
+    languages.python = {
+      enable = true;
+      poetry = {
+        enable = true;
+        activate.enable = true;
+      };
+    };
+
+    languages.rust = {
+      enable = true;
+      channel = "stable";
+    };
   };
 }
