bf070ab35360a0c2dbd5d237582642991152be19
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2024-07-30 13:37:35 +0100

    feat: Move flag parsing into arg_from_ast (#349)
    Closes #347 and closes #312

--- guppylang/definition/ty.py
+++ guppylang/definition/ty.py
@@ -1,18 +1,22 @@
 from abc import abstractmethod
 from collections.abc import Callable, Sequence
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypeAlias
 
 from hugr.serialization import tys
 
 from guppylang.ast_util import AstNode
 from guppylang.definition.common import CompiledDef, Definition
+from guppylang.error import GuppyError
 from guppylang.tys.arg import Argument
 from guppylang.tys.param import Parameter, check_all_args
-from guppylang.tys.ty import OpaqueType, Type
+from guppylang.tys.ty import InputFlags, OpaqueType, Type
 
 if TYPE_CHECKING:
     from guppylang.checker.core import Globals
+
+
+FlaggedArgs: TypeAlias = Sequence[tuple[Argument, InputFlags]]
 
 
 @dataclass(frozen=True)
@@ -23,7 +27,7 @@ class TypeDef(Definition):
 
     @abstractmethod
     def check_instantiate(
-        self, args: Sequence[Argument], globals: "Globals", loc: AstNode | None = None
+        self, args: FlaggedArgs, globals: "Globals", loc: AstNode | None = None
     ) -> Type:
         """Checks if the type definition can be instantiated with the given arguments.
 
@@ -42,12 +46,23 @@ class OpaqueTypeDef(TypeDef, CompiledDef):
     bound: tys.TypeBound | None = None
 
     def check_instantiate(
-        self, args: Sequence[Argument], globals: "Globals", loc: AstNode | None = None
+        self, args: FlaggedArgs, globals: "Globals", loc: AstNode | None = None
     ) -> OpaqueType:
         """Checks if the type definition can be instantiated with the given arguments.
 
         Returns the resulting concrete type or raises a user error if the arguments are
         invalid.
         """
+        args = check_no_flags(args, loc)
         check_all_args(self.params, args, self.name, loc)
         return OpaqueType(args, self)
+
+
+def check_no_flags(args: FlaggedArgs, loc: AstNode | None) -> list[Argument]:
+    """Checks that no argument to `check_instantiate` has any `@flags`."""
+    for _, flags in args:
+        if flags != InputFlags.NoFlags:
+            raise GuppyError(
+                "`@` type annotations are not allowed in this position", loc
+            )
+    return [ty for ty, _ in args]
