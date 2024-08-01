1b64cd6f435f1097a601ea75af74a81ffeb63c80
Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
2024-08-01 11:32:50 +0100

    test: guppy -> pytket lowering with tket2 (#348)
    Adds the tests removed from tket2 in
    https://github.com/CQCL/tket2/pull/516
    
    This requires a tket2 alpha release to avoid the git ref dependency.

--- tests/util.py
+++ tests/util.py
@@ -1,6 +1,17 @@
+from typing import TYPE_CHECKING, Any
+
 import guppylang
+from guppylang.definition.function import RawFunctionDef
 from guppylang.hugr_builder.hugr import Hugr
 from guppylang.module import GuppyModule
+
+if TYPE_CHECKING:
+    try:
+        from tket2.circuit import (
+            Tk2Circuit,  # type: ignore[import-untyped, import-not-found, unused-ignore]
+        )
+    except ImportError:
+        Tk2Circuit = Any
 
 
 def compile_guppy(fn) -> Hugr:
@@ -28,3 +39,18 @@ def dump_llvm(hugr: Hugr):
 
     except ImportError:
         pass
+
+
+def guppy_to_circuit(guppy_func: RawFunctionDef) -> "Tk2Circuit":
+    """Convert a Guppy function definition to a `Tk2Circuit`."""
+    # TODO: Should this be part of the guppy API?
+    from tket2.circuit import Tk2Circuit
+
+    module = guppy_func.id.module
+    assert module is not None, "Function definition must belong to a module"
+
+    hugr = module.compile()
+    assert hugr is not None, "Module must be compilable"
+
+    json = hugr.to_raw().to_json()
+    return Tk2Circuit.from_guppy_json(json, guppy_func.name)
