7d5106cd59ad452046d0dfffd10eea9d9b617431
Craig Roy <croyzor@users.noreply.github.com>
2024-08-06 15:15:56 +0100

    test: Add a T factory example program (#360)

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -5,3 +5,9 @@ def test_random_walk_qpe(validate):
     from examples.random_walk_qpe import hugr
 
     validate(hugr)
+
+
+def test_t_factory(validate):
+    from examples.t_factory import hugr
+
+    validate(hugr)
