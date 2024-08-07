15f2ea7573429b198f4879390ce0f8e1e3c60750
Agustin Borgna <agustin.borgna@quantinuum.com>
2024-08-07 18:59:47 +0100

    wip: fix cyclic import

--- guppylang/definition/value.py
+++ guppylang/definition/value.py
@@ -6,7 +6,6 @@ from typing import TYPE_CHECKING, Any
 from hugr import Wire
 
 from guppylang.ast_util import AstNode
-from guppylang.compiler.core import DFContainer
 from guppylang.definition.common import CompiledDef, Definition
 from guppylang.error import GuppyError
 from guppylang.tys.subst import Inst, Subst
@@ -14,7 +13,7 @@ from guppylang.tys.ty import FunctionType, Type
 
 if TYPE_CHECKING:
     from guppylang.checker.core import Context
-    from guppylang.compiler.core import CompiledGlobals
+    from guppylang.compiler.core import CompiledGlobals, DFContainer
 
 
 @dataclass(frozen=True)
@@ -31,7 +30,9 @@ class CompiledValueDef(ValueDef, CompiledDef):
     """Abstract base class for compiled definitions that represent values."""
 
     @abstractmethod
-    def load(self, dfg: DFContainer, globals: "CompiledGlobals", node: AstNode) -> Wire:
+    def load(
+        self, dfg: "DFContainer", globals: "CompiledGlobals", node: AstNode
+    ) -> Wire:
         """Loads the defined value into a local Hugr dataflow graph."""
 
 
@@ -67,7 +68,7 @@ class CompiledCallableDef(CallableDef, CompiledValueDef):
         self,
         args: list[Wire],
         type_args: Inst,
-        dfg: DFContainer,
+        dfg: "DFContainer",
         globals: "CompiledGlobals",
         node: AstNode,
     ) -> list[Wire]:
@@ -77,7 +78,7 @@ class CompiledCallableDef(CallableDef, CompiledValueDef):
     def load_with_args(
         self,
         type_args: Inst,
-        dfg: DFContainer,
+        dfg: "DFContainer",
         globals: "CompiledGlobals",
         node: AstNode,
     ) -> Wire:
@@ -86,6 +87,8 @@ class CompiledCallableDef(CallableDef, CompiledValueDef):
         Requires an instantiation for all function parameters.
         """
 
-    def load(self, dfg: DFContainer, globals: "CompiledGlobals", node: AstNode) -> Wire:
+    def load(
+        self, dfg: "DFContainer", globals: "CompiledGlobals", node: AstNode
+    ) -> Wire:
         """Loads the defined value into a local Hugr dataflow graph."""
         return self.load_with_args([], dfg, globals, node)
