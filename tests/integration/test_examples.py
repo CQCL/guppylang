c2ab65f5bc07d3d710a733d66880beb377137a0b
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2024-08-21 17:46:03 +0100

    test: Add test to detect notebook regressions (#383)
    Closes #375

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -1,6 +1,11 @@
 """Tests validating the files in the `examples` directory."""
 
 
+def test_demo_notebook(nb_regression):
+    nb_regression.diff_ignore += ("/metadata/language_info/version",)
+    nb_regression.check("examples/demo.ipynb")
+
+
 def test_random_walk_qpe(validate):
     from examples.random_walk_qpe import hugr
 
