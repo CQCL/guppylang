0ada32b28c1d7c5a7e1039e148a223ba4da95d74
Mark Koch <mark.koch@quantinuum.com>
2024-08-28 12:18:37 +0100

    feat: Update linearity checker to handle subscripts

---
+++ tests/error/array_errors/subscript_non_inout.py
@@ -0,0 +1,18 @@
+import guppylang.prelude.quantum as quantum
+from guppylang.decorator import guppy
+from guppylang.module import GuppyModule
+from guppylang.prelude.builtins import array
+from guppylang.prelude.quantum import qubit
+
+
+module = GuppyModule("test")
+module.load(quantum)
+
+
+@guppy(module)
+def main(qs: array[qubit, 42]) -> tuple[qubit, array[qubit, 42]]:
+    q = qs[0]
+    return q, qs
+
+
+module.compile()
