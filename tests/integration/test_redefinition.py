ad78e7e4716395c7ef5b13b2ee1a37916407ba1c
Mark Koch <mark.koch@quantinuum.com>
2024-09-05 14:23:40 +0200

    feat: Skip checking of redefined functions

--- tests/integration/test_redefinition.py
+++ tests/integration/test_redefinition.py
@@ -1,19 +1,83 @@
+import pytest
+
 from guppylang.decorator import guppy
 from guppylang.module import GuppyModule
 
-import guppylang.prelude.quantum as quantum
 
-
-def test_redefinition(validate):
+def test_func_redefinition(validate):
     module = GuppyModule("test")
-    module.load_all(quantum)
 
     @guppy(module)
     def test() -> bool:
-        return True
+        return 5  # Type error on purpose
 
     @guppy(module)
     def test() -> bool:  # noqa: F811
         return False
 
     validate(module.compile())
+
+
+def test_method_redefinition(validate):
+    module = GuppyModule("test")
+
+    @guppy.struct(module)
+    class Test:
+        x: int
+
+        @guppy(module)
+        def foo(self: "Test") -> int:
+            return 1.0  # Type error on purpose
+
+        @guppy(module)
+        def foo(self: "Test") -> int:
+            return 1  # Type error on purpose
+
+    validate(module.compile())
+
+
+@pytest.mark.skip("See https://github.com/CQCL/guppylang/issues/456")
+def test_struct_redefinition(validate):
+    module = GuppyModule("test")
+
+    @guppy.struct(module)
+    class Test:
+        x: "blah"  # Non-existing type
+
+    @guppy.struct(module)
+    class Test:
+        y: int
+
+    @guppy(module)
+    def main(x: int) -> Test:
+        return Test(x)
+
+    validate(module.compile())
+
+
+@pytest.mark.skip("See https://github.com/CQCL/guppylang/issues/456")
+def test_struct_method_redefinition(validate):
+    module = GuppyModule("test")
+
+    @guppy.struct(module)
+    class Test:
+        x: int
+
+        @guppy(module)
+        def foo(self: "Test") -> int:
+            return 1.0  # Type error on purpose
+
+    @guppy.struct(module)
+    class Test:
+        y: int
+
+        @guppy(module)
+        def bar(self: "Test") -> int:
+            return self.y
+
+    @guppy(module)
+    def main(x: int) -> int:
+        return Test(x).bar()
+
+    validate(module.compile())
+
