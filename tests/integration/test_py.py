27f67626161a1c5e944bcc56d625ca16e6d27f63
Agustin Borgna <agustinborgna@gmail.com>
2024-09-11 11:25:42 +0200

    chore: Unskip now-passing tests

--- tests/integration/test_py.py
+++ tests/integration/test_py.py
@@ -73,9 +73,6 @@ def test_tuple_implicit(validate):
     validate(foo)
 
 
-@pytest.mark.skip(
-    reason="collections extensions not defined in the validator. Remove once updated to hugr 0.8",
-)
 def test_list_basic(validate):
     @compile_guppy
     def foo() -> list[int]:
@@ -85,9 +82,6 @@ def test_list_basic(validate):
     validate(foo)
 
 
-@pytest.mark.skip(
-    reason="collections extensions not defined in the validator. Remove once updated to hugr 0.8",
-)
 def test_list_empty(validate):
     @compile_guppy
     def foo() -> list[int]:
@@ -96,9 +90,6 @@ def test_list_empty(validate):
     validate(foo)
 
 
-@pytest.mark.skip(
-    reason="collections extensions not defined in the validator. Remove once updated to hugr 0.8",
-)
 def test_list_empty_nested(validate):
     @compile_guppy
     def foo() -> None:
@@ -107,9 +98,6 @@ def test_list_empty_nested(validate):
     validate(foo)
 
 
-@pytest.mark.skip(
-    reason="collections extensions not defined in the validator. Remove once updated to hugr 0.8",
-)
 def test_list_empty_multiple(validate):
     @compile_guppy
     def foo() -> None:
@@ -119,7 +107,6 @@ def test_list_empty_multiple(validate):
 
 
 @pytest.mark.skipif(not tket2_installed, reason="Tket2 is not installed")
-@pytest.mark.skip("Fails because of extensions in types #343")
 def test_pytket_single_qubit(validate):
     from pytket import Circuit
 
@@ -138,7 +125,6 @@ def test_pytket_single_qubit(validate):
 
 
 @pytest.mark.skipif(not tket2_installed, reason="Tket2 is not installed")
-@pytest.mark.skip("Fails because of extensions in types #343")
 def test_pytket_multi_qubit(validate):
     from pytket import Circuit
 
