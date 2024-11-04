c5adfda83ab3722882e19dd9f9079339ce67815d
Mark Koch <mark.koch@quantinuum.com>
2024-11-04 14:19:37 +0000

    Add non static test

--- tests/integration/test_range.py
+++ tests/integration/test_range.py
@@ -20,7 +20,16 @@ def test_range(validate, run_int_fn):
             total += 100 + x
         return total
 
+    @guppy(module)
+    def non_static() -> int:
+        total = 0
+        n = 4
+        for x in range(n + 1):
+            total += x + 100  # Make the initial 0 obvious
+        return total
+
     compiled = module.compile()
     validate(compiled)
     run_int_fn(compiled, expected=510)
     run_int_fn(compiled, expected=0, fn_name="negative")
+    run_int_fn(compiled, expected=510, fn_name="non_static")
