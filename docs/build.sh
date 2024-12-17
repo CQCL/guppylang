1a588cccf39a32bea5f08dee896b49ab51ed6f4b
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-12-17 10:55:50 +0000

    fix: docs build command

--- docs/build.sh
+++ docs/build.sh
@@ -4,4 +4,4 @@ mkdir build
 
 touch build/.nojekyll  # Disable jekyll to keep files starting with underscores
 
-uv run --extra docs sphinx-build -b html ./api-docs ./build/api-docs
+uv run --group docs sphinx-build -b html ./api-docs ./build/api-docs
