647ad3dae09239782fc96c5c97218ecfa400e2fc
Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
2024-12-20 10:19:12 +0000

    chore: Attempt at making execute_llvm editable in local `uv` development (#737)
    This is an attempt at avoiding `uv` from caching `execute_llvm` and
    reflecting new changes to the package.
    
    As part of this I updated various dependencies:
    
    - `maturin 1.4 -> 1.7.7`, since [this
    issue](https://www.github.com/PyO3/maturin/issues/2346) seemed related.
    - `pyo3 0.19 -> 0.23`, and updated the bindings syntax.
    
    The change that actually worked was removing the unversioned
    "execute_llvm" entry in the main package's dependencies. Since we
    manually install it using `maturin develop`, it now ensures that changes
    get re-compiled and reflected on the python runtime.
    
    drive-by: Update `execute_llvm` to read packages instead of hugrs, so we
    can pass it guppys with quantum primitives.

--- execute_llvm/src/lib.rs
+++ execute_llvm/src/lib.rs
@@ -1,5 +1,8 @@
 //! This module provides a Python interface to compile and execute a Hugr program to LLVM IR.
+use hugr::llvm::custom::CodegenExtsMap;
 use hugr::llvm::utils::fat::FatExt;
+use hugr::llvm::CodegenExtsBuilder;
+use hugr::package::Package;
 use hugr::Hugr;
 use hugr::{self, ops, std_extensions, HugrView};
 use inkwell::{context::Context, module::Module, values::GenericValue};
@@ -11,8 +14,9 @@ macro_rules! pyerr {
 }
 
 fn parse_hugr(hugr_json: &str) -> PyResult<hugr::Hugr> {
-    let hugr = Hugr::load_json(hugr_json.as_bytes(), &std_extensions::std_reg())
+    let mut pkg = Package::from_json(hugr_json, &std_extensions::std_reg())
         .map_err(|e| pyerr!("Couldn't deserialize hugr: {}", e))?;
+    let hugr = std::mem::take(&mut pkg.modules[0]);
     Ok(hugr)
 }
 
@@ -38,9 +42,22 @@ fn find_funcdef_node(hugr: impl HugrView, fn_name: &str) -> PyResult<hugr::Node>
     }
 }
 
-fn guppy_pass(mut hugr: Hugr) -> Hugr  {
-    hugr::algorithms::MonomorphizePass::default().run(&mut hugr).unwrap();
+fn guppy_pass(mut hugr: Hugr) -> Hugr {
+    hugr::algorithms::MonomorphizePass::default()
+        .run(&mut hugr)
+        .unwrap();
     hugr::algorithms::remove_polyfuncs(hugr)
+}
+
+fn codegen_extensions() -> CodegenExtsMap<'static, Hugr> {
+    CodegenExtsBuilder::default()
+        .add_default_prelude_extensions()
+        .add_int_extensions()
+        .add_float_extensions()
+        .add_conversion_extensions()
+        .add_logic_extensions()
+        .add_default_array_extensions()
+        .finish()
 }
 
 fn compile_module<'a>(
@@ -50,33 +67,16 @@ fn compile_module<'a>(
 ) -> PyResult<Module<'a>> {
     let llvm_module = ctx.create_module("guppy_llvm");
     // TODO: Handle tket2 codegen extension
-    let extensions = hugr::llvm::custom::CodegenExtsBuilder::default()
-        .add_int_extensions()
-        .add_logic_extensions()
-        .add_default_prelude_extensions()
-        .add_default_array_extensions()
-        .add_float_extensions()
-        .add_conversion_extensions();
+    let extensions = codegen_extensions();
 
     let emitter =
-        hugr::llvm::emit::EmitHugr::new(ctx, llvm_module, namer.into(), extensions.finish().into());
+        hugr::llvm::emit::EmitHugr::new(ctx, llvm_module, namer.into(), extensions.into());
     let hugr_module = hugr.fat_root().unwrap();
     let emitter = emitter
         .emit_module(hugr_module)
         .map_err(|e| pyerr!("Error compiling to llvm: {}", e))?;
 
     Ok(emitter.finish())
-}
-
-#[pyfunction]
-fn compile_module_to_string(hugr_json: &str) -> PyResult<String> {
-    let mut hugr = parse_hugr(hugr_json)?;
-    let ctx = Context::create();
-
-    hugr = guppy_pass(hugr);
-    let module = compile_module(&hugr, &ctx, Default::default())?;
-
-    Ok(module.print_to_string().to_str().unwrap().to_string())
 }
 
 fn run_function<T>(
@@ -105,27 +105,38 @@ fn run_function<T>(
     parse_result(&ctx, llvm_result)
 }
 
-#[pyfunction]
-fn run_int_function(hugr_json: &str, fn_name: &str) -> PyResult<i64> {
-    run_function::<i64>(hugr_json, fn_name, |_, llvm_val| {
-        // GenericVal is 64 bits wide
-        let int_with_sign = llvm_val.as_int(true);
-        let signed_int = int_with_sign as i64;
-        Ok(signed_int)
-    })
-}
-
-#[pyfunction]
-fn run_float_function(hugr_json: &str, fn_name: &str) -> PyResult<f64> {
-    run_function::<f64>(hugr_json, fn_name, |ctx, llvm_val| {
-        Ok(llvm_val.as_float(&ctx.f64_type()))
-    })
-}
-
 #[pymodule]
-fn execute_llvm(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(compile_module_to_string, m)?)?;
-    m.add_function(wrap_pyfunction!(run_int_function, m)?)?;
-    m.add_function(wrap_pyfunction!(run_float_function, m)?)?;
-    Ok(())
+mod execute_llvm {
+    use inkwell::context::Context;
+    use pyo3::pyfunction;
+
+    use super::*;
+
+    #[pyfunction]
+    fn compile_module_to_string(hugr_json: &str) -> PyResult<String> {
+        let mut hugr = parse_hugr(hugr_json)?;
+        let ctx = Context::create();
+
+        hugr = guppy_pass(hugr);
+        let module = compile_module(&hugr, &ctx, Default::default())?;
+
+        Ok(module.print_to_string().to_str().unwrap().to_string())
+    }
+
+    #[pyfunction]
+    fn run_int_function(hugr_json: &str, fn_name: &str) -> PyResult<i64> {
+        run_function::<i64>(hugr_json, fn_name, |_, llvm_val| {
+            // GenericVal is 64 bits wide
+            let int_with_sign = llvm_val.as_int(true);
+            let signed_int = int_with_sign as i64;
+            Ok(signed_int)
+        })
+    }
+
+    #[pyfunction]
+    fn run_float_function(hugr_json: &str, fn_name: &str) -> PyResult<f64> {
+        run_function::<f64>(hugr_json, fn_name, |ctx, llvm_val| {
+            Ok(llvm_val.as_float(&ctx.f64_type()))
+        })
+    }
 }
