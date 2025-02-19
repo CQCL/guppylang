727c4f2f7ff91be0257e01b617d80fdb046f83f7
Melf <cqc@melf.de>
2025-02-19 11:11:14 +0000

    update devenv.nix, add cc.lib to path

--- devenv.nix
+++ devenv.nix
@@ -36,6 +36,8 @@
 
   env.LLVM_SYS_140_PREFIX = pkgs.llvmPackages_14.libllvm.dev;
 
+  env.LD_LIBRARY_PATH = "${lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.zlib ]}";
+
   languages.rust = {
     enable = true;
     channel = "stable";
