eb4b895b90b12a9833ebf27933c981452d91f181
Kartik Singhal <kartik.singhal@quantinuum.com>
2025-02-26 08:51:53 -0600

    fix: use int_arg() while defining int args in external_op

--- guppylang/std/qsystem/random.py
+++ guppylang/std/qsystem/random.py
@@ -6,7 +6,7 @@ from guppylang.std._internal.compiler.quantum import (
     QSYSTEM_RANDOM_EXTENSION,
     RNGCONTEXT_T,
 )
-from guppylang.std._internal.util import external_op
+from guppylang.std._internal.util import external_op, int_arg
 from guppylang.std.builtins import owned
 
 qsystem_random = GuppyModule("qsystem.random")
@@ -38,7 +38,7 @@ class Random:  # type: ignore[operator] # "RawStructDef" not callable
 @guppy.type(RNGCONTEXT_T, copyable=False, module=qsystem_random)
 class RNGContext:
     @guppy.hugr_op(
-        external_op("NewRNGContext", [int], ext=QSYSTEM_RANDOM_EXTENSION),
+        external_op("NewRNGContext", [int_arg()], ext=QSYSTEM_RANDOM_EXTENSION),
         module=qsystem_random,
     )
     @no_type_check
@@ -51,7 +51,7 @@ class RNGContext:
     # def init_safe(seed: int) -> "RNGContext" | None:
 
     @guppy.hugr_op(
-        external_op("DeleteRNGContext", [int], ext=QSYSTEM_RANDOM_EXTENSION),
+        external_op("DeleteRNGContext", [], ext=QSYSTEM_RANDOM_EXTENSION),
         module=qsystem_random,
     )
     @no_type_check
@@ -74,7 +74,7 @@ class RNGContext:
 
     @guppy.hugr_op(
         external_op(
-            "RandomIntBounded", [RNGCONTEXT_T, int], ext=QSYSTEM_RANDOM_EXTENSION
+            "RandomIntBounded", [RNGCONTEXT_T, int_arg()], ext=QSYSTEM_RANDOM_EXTENSION
         ),
         module=qsystem_random,
     )
