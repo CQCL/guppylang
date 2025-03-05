02e7759fecaa611c2e46fb43dc733ffa23850b69
Mark Koch <mark.koch@quantinuum.com>
2025-03-05 14:49:34 +0000

    feat!: Switch to improved iterator protocol

--- guppylang/std/option.py
+++ guppylang/std/option.py
@@ -1,81 +1,68 @@
-from collections.abc import Sequence
+from pathlib import Path
 from typing import Generic, no_type_check
 
-import hugr.tys as ht
-
-from guppylang.decorator import guppy
-from guppylang.error import InternalGuppyError
+from guppylang.decorator import ModuleIdentifier, guppy
+from guppylang.std import builtins
 from guppylang.std._internal.compiler.option import (
     OptionConstructor,
     OptionTestCompiler,
     OptionUnwrapCompiler,
     OptionUnwrapNothingCompiler,
 )
-from guppylang.std.builtins import mem_swap, owned
-from guppylang.tys.arg import Argument, TypeArg
-from guppylang.tys.param import TypeParam
+from guppylang.std.builtins import L, mem_swap, owned
+from guppylang.tys.builtin import option_type_def
 
-
-def _option_to_hugr(args: Sequence[Argument]) -> ht.Type:
-    match args:
-        case [TypeArg(ty)]:
-            return ht.Option(ty.to_hugr())
-        case _:
-            raise InternalGuppyError("Invalid type args for Option")
-
-
-T = guppy.type_var("T", copyable=False, droppable=False)
-
-
-@guppy.type(
-    _option_to_hugr,
-    params=[TypeParam(0, "T", must_be_copyable=False, must_be_droppable=False)],
+builtins_module = guppy.get_module(
+    ModuleIdentifier(Path(builtins.__file__), "builtins", builtins)
 )
-class Option(Generic[T]):  # type: ignore[misc]
+
+
+@guppy.extend_type(option_type_def, module=builtins_module)
+class Option(Generic[L]):  # type: ignore[misc]
     """Represents an optional value."""
 
-    @guppy.custom(OptionTestCompiler(0))
+    @guppy.custom(OptionTestCompiler(0), module=builtins_module)
     @no_type_check
-    def is_nothing(self: "Option[T]") -> bool:
+    def is_nothing(self: "Option[L]") -> bool:
         """Returns `True` if the option is a `nothing` value."""
 
-    @guppy.custom(OptionTestCompiler(1))
+    @guppy.custom(OptionTestCompiler(1), module=builtins_module)
     @no_type_check
-    def is_some(self: "Option[T]") -> bool:
+    def is_some(self: "Option[L]") -> bool:
         """Returns `True` if the option is a `some` value."""
 
-    @guppy.custom(OptionUnwrapCompiler())
+    @guppy.custom(OptionUnwrapCompiler(), module=builtins_module)
     @no_type_check
-    def unwrap(self: "Option[T]" @ owned) -> T:
+    def unwrap(self: "Option[L]" @ owned) -> L:
         """Returns the contained `some` value, consuming `self`.
 
         Panics if the option is a `nothing` value.
         """
 
-    @guppy.custom(OptionUnwrapNothingCompiler())
+    @guppy.custom(OptionUnwrapNothingCompiler(), module=builtins_module)
     @no_type_check
-    def unwrap_nothing(self: "Option[T]" @ owned) -> None:
+    def unwrap_nothing(self: "Option[L]" @ owned) -> None:
         """Returns `None` if the option is a `nothing` value, consuming `self`.
 
         Panics if the option is a `some` value.
         """
 
-    @guppy
+    @guppy(builtins_module)
     @no_type_check
-    def take(self: "Option[T]") -> "Option[T]":
+    def take(self: "Option[L]") -> "Option[L]":
         """Swaps the value of `self` with `nothing` and returns the original value."""
-        n: Option[T] = nothing()  # type: ignore[type-arg, valid-type]
+        n: Option[L] = nothing()  # type: ignore[type-arg, valid-type]
         mem_swap(n, self)
         return n
 
 
-@guppy.custom(OptionConstructor(0))
+@guppy.custom(OptionConstructor(0), module=builtins_module)
 @no_type_check
-def nothing() -> Option[T]:
+def nothing() -> Option[L]:
     """Constructs a `nothing` optional value."""
 
 
-@guppy.custom(OptionConstructor(1))
+@guppy.custom(OptionConstructor(1), module=builtins_module)
 @no_type_check
-def some(value: T @ owned) -> Option[T]:
+def some(value: L @ owned) -> Option[L]:
     """Constructs a `some` optional value."""
