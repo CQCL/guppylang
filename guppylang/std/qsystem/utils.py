d03a8ff49e60bb73de1773f9b4473e74035fb301
Louis Narmour <louis.narmour@quantinuum.com>
2025-03-05 23:02:46 +0000

    feat: add order_in_zones tket2 extension

--- guppylang/std/qsystem/utils.py
+++ guppylang/std/qsystem/utils.py
@@ -2,14 +2,22 @@ from typing import no_type_check
 
 from guppylang.decorator import guppy
 from guppylang.module import GuppyModule
+from guppylang.std.builtins import array
+from guppylang.std.quantum import qubit
 from guppylang.std._internal.compiler.quantum import QSYSTEM_UTILS_EXTENSION
 from guppylang.std._internal.util import external_op
 
 qsystem_utils = GuppyModule("qsystem.utils")
-
+qsystem_utils.load(qubit)
 
 @guppy.hugr_op(
     external_op("GetCurrentShot", [], ext=QSYSTEM_UTILS_EXTENSION), module=qsystem_utils
 )
 @no_type_check
 def get_current_shot() -> int: ...
+
+@guppy.hugr_op(
+    external_op("OrderInZones", [], ext=QSYSTEM_UTILS_EXTENSION), module=qsystem_utils
+)
+@no_type_check
+def order_in_zones(qubits: array[qubit, 16]) -> None: ...
