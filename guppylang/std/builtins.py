b56d66c25ec6889619def8cf4f417fc3bdf19054
tatiana-s <tatiana.sedelnikov@quantinuum.com>
2025-03-18 09:17:15 +0000

    feat: Implement `float` to `int` and `nat` casts (#831)
    Closes #794

--- guppylang/std/builtins.py
+++ guppylang/std/builtins.py
@@ -33,7 +33,7 @@ from guppylang.std._internal.compiler.list import (
     ListPushCompiler,
     ListSetitemCompiler,
 )
-from guppylang.std._internal.compiler.prelude import MemSwapCompiler
+from guppylang.std._internal.compiler.prelude import MemSwapCompiler, UnwrapOpCompiler
 from guppylang.std._internal.util import (
     float_op,
     int_op,
@@ -469,7 +469,12 @@ class Float:
     @guppy.hugr_op(float_op("fgt"))
     def __gt__(self: float, other: float) -> bool: ...
 
-    @guppy.hugr_op(unsupported_op("trunc_s"))  # TODO `trunc_s` returns an option
+    @guppy.custom(
+        UnwrapOpCompiler(
+            # Use `int_op` to instantiate type arg with 64-bit integer.
+            int_op("trunc_s", hugr.std.int.CONVERSIONS_EXTENSION),
+        )
+    )
     def __int__(self: float) -> int: ...
 
     @guppy.hugr_op(float_op("fle"))
@@ -486,7 +491,12 @@ class Float:
     @guppy.hugr_op(float_op("fmul"))
     def __mul__(self: float, other: float) -> float: ...
 
-    @guppy.hugr_op(unsupported_op("trunc_u"))  # TODO `trunc_u` returns an option
+    @guppy.custom(
+        UnwrapOpCompiler(
+            # Use `int_op` to instantiate type arg with 64-bit integer.
+            int_op("trunc_u", hugr.std.int.CONVERSIONS_EXTENSION),
+        )
+    )
     def __nat__(self: float) -> nat: ...
 
     @guppy.hugr_op(float_op("fne"))
