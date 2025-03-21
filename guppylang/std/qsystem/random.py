b8c66da878576af6fb1b8ab8f2318e812194e317
Kartik Singhal <kartik.singhal@quantinuum.com>
2025-03-21 12:06:11 -0500

    docs: add wrappers to provide docstrings
    workaround until #826 is resolved

--- guppylang/std/qsystem/random.py
+++ guppylang/std/qsystem/random.py
@@ -1,3 +1,4 @@
+# mypy: disable-error-code="no-any-return"
 from typing import no_type_check
 
 from guppylang.decorator import guppy
@@ -32,26 +33,50 @@ class RNG:
     @guppy(qsystem_random)  # type: ignore[misc] # Unsupported decorated constructor type; Self argument missing for a non-static method (or an invalid type for self)
     def __new__(seed: int) -> "RNG":
         """Create a new random number generator using a seed."""
-        return _new_rng_context(seed).unwrap()  # type: ignore[no-any-return] # Returning Any from function declared to return "RNGContext"
+        return _new_rng_context(seed).unwrap()
+
+    @guppy(qsystem_random)
+    def discard(self: "RNG" @ owned) -> None:  # type: ignore[valid-type] # Invalid type comment or annotation
+        """Discard the random number generator."""
+        self._discard()
+
+    @guppy(qsystem_random)
+    def random_int(self: "RNG") -> int:
+        """Generate a random 32-bit signed integer."""
+        return self._random_int()
+
+    @guppy(qsystem_random)
+    def random_float(self: "RNG") -> float:
+        """Generate a random floating point value in the range [0,1)."""
+        return self._random_float()
+
+    @guppy(qsystem_random)
+    def random_int_bounded(self: "RNG", bound: int) -> int:
+        """Generate a random 32-bit integer in the range [0, bound).
+
+        Args:
+            bound: The upper bound of the range, needs to less than 2^31.
+        """
+        return self._random_int_bounded(bound)
 
     @guppy.hugr_op(
         external_op("DeleteRNGContext", [], ext=QSYSTEM_RANDOM_EXTENSION),
         module=qsystem_random,
     )
     @no_type_check
-    def discard(self: "RNG" @ owned) -> None: ...
+    def _discard(self: "RNG" @ owned) -> None: ...
 
     @guppy.custom(RandomIntCompiler(), module=qsystem_random)
     @no_type_check
-    def random_int(self: "RNG") -> int: ...
+    def _random_int(self: "RNG") -> int: ...
 
     @guppy.hugr_op(
         external_op("RandomFloat", [], ext=QSYSTEM_RANDOM_EXTENSION),
         module=qsystem_random,
     )
     @no_type_check
-    def random_float(self: "RNG") -> float: ...
+    def _random_float(self: "RNG") -> float: ...
 
     @guppy.custom(RandomIntBoundedCompiler(), module=qsystem_random)
     @no_type_check
-    def random_int_bounded(self: "RNG", bound: int) -> int: ...
+    def _random_int_bounded(self: "RNG", bound: int) -> int: ...
