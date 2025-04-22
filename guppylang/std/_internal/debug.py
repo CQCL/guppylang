e89c62d49f7c3b7224450f2d950f6613227a9226
Tatiana S <tatiana.sedelnikov@quantinuum.com>
2025-04-22 11:13:49 +0100

    Minor fixes

--- guppylang/std/_internal/debug.py
+++ guppylang/std/_internal/debug.py
@@ -4,10 +4,11 @@ from typing import ClassVar
 
 from guppylang.ast_util import with_loc
 from guppylang.checker.errors.generic import ExpectedError
+from guppylang.checker.errors.type_errors import WrongNumberOfArgsError
 from guppylang.checker.expr_checker import ExprChecker, ExprSynthesizer, synthesize_call
 from guppylang.definition.custom import CustomCallChecker
 from guppylang.definition.ty import TypeDef
-from guppylang.diagnostic import Error, Note
+from guppylang.diagnostic import Error
 from guppylang.error import GuppyTypeError
 from guppylang.nodes import StateResultExpr
 from guppylang.std._internal.checker import TAG_MAX_LEN, TooLongError
@@ -29,15 +30,6 @@ class StateResultChecker(CustomCallChecker):
         span_label: ClassVar[str] = (
             "Qubits whose state should be reported must be passed explicitly"
         )
-
-    @dataclass(frozen=True)
-    class MoreThanOneArrayError(Error):
-        title: ClassVar[str] = "Too many array arguments"
-        span_label: ClassVar[str] = "Only one array argument is allowed"
-
-        @dataclass(frozen=True)
-        class Suggestion(Note):
-            message: ClassVar[str] = "Consider passing separate qubits"
 
     def synthesize(self, args: list[ast.expr]) -> tuple[ast.expr, Type]:
         tag, _ = ExprChecker(self.ctx).check(args[0], string_type())
@@ -62,8 +54,7 @@ class StateResultChecker(CustomCallChecker):
         arg, ty = ExprSynthesizer(self.ctx).synthesize(args[1])
         if is_array_type(ty):
             if len(args) > 2:
-                err = self.MoreThanOneArrayError(self.node)
-                err.add_sub_diagnostic(self.MoreThanOneArrayError.Suggestion(None))
+                err = WrongNumberOfArgsError(args[2], 2, len(args))
                 raise GuppyTypeError(err)
             element_ty = get_element_type(ty)
             if not element_ty == qubit_ty:
@@ -80,8 +71,6 @@ class StateResultChecker(CustomCallChecker):
         else:
             for arg in args[1:]:
                 qbt, _ = ExprChecker(self.ctx).check(arg, qubit_ty)
-                if not ExprChecker(self.ctx).check(arg, qubit_ty):
-                    raise GuppyTypeError(ExpectedError(arg, "a qubit"))
                 syn_args.append(qbt)
             func_ty = FunctionType(
                 [FuncInput(string_type(), InputFlags.NoFlags)]
