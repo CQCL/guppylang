11ac5645328e3a2cb749aa85147f635c18ce0f51
Tatiana S <tatiana.sedelnikov@quantinuum.com>
2025-05-06 15:13:43 +0100

    Add iterator example

---
+++ tests/error/inout_errors/moved_iterator.py
@@ -0,0 +1,20 @@
+from guppylang.decorator import guppy
+from guppylang.module import GuppyModule
+from guppylang.std.builtins import array, owned
+from guppylang.std.quantum import qubit
+
+module = GuppyModule("test")
+module.load(qubit)
+
+
+@guppy.declare(module)
+def use(q: array[int, 3] @owned) -> None: ...
+
+
+@guppy(module)
+def test(q: array[int, 3]) -> None:
+    for i in q:
+        use(q)
+
+
+module.compile()
