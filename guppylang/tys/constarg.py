c6d66d8004be9ac22bcee61d6a79634721de4b44
Craig Roy <craig.roy@quantinuum.com>
2025-05-20 14:36:28 +0200

    Add ConstStringArg

---
+++ guppylang/tys/constarg.py
@@ -0,0 +1,15 @@
+from hugr import tys as ht
+
+from guppylang.tys.arg import ConstArg
+from guppylang.tys.builtin import is_string_type
+from guppylang.tys.const import ConstValue
+
+
+class ConstStringArg(ConstArg):
+    def to_hugr(self) -> ht.TypeArg:
+        match self.const:
+            case ConstValue(value=v, ty=ty) if is_string_type(ty):
+                assert isinstance(v, str)
+                return ht.StringArg(v)
+            case _:
+                return super().to_hugr()
