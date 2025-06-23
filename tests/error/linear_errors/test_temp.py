a9ba067cc0110a71056c97a7eaf4fcac667bd027
Tatiana S <tatiana.sedelnikov@quantinuum.com>
2025-06-23 13:37:33 +0100

    Remove temp test

--- tests/error/linear_errors/test_temp.py
+++
@@ -1,9 +0,0 @@
-from tests.util import compile_guppy
-from guppylang.std.quantum import qubit, x
-
-
-@compile_guppy
-def foo() -> None:
-    t = (qubit(), qubit(), 0)
-    while True:
-        x(t[0])
