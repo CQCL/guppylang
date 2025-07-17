32a4bbced00b4603c2c4d04af3c35e40bcac427f
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2025-07-17 18:06:53 +0100

    docs: add quantum and qsystem gate definitions (#912)
    Adding some gate definitions and doctests to the qsystem and quantum
    docs
    
    added docstrings for everything along with matrix definitions of the
    gates (as in pytket
    [OpType](https://docs.quantinuum.com/tket/api-docs/optype.html))
    
    Theres quite a lot of freedom in convention for how we respresent this
    (captialisation etc). Would be interested in your thoughts.
    
    Regarding functional gates I've just mentioned that they are the same as
    the std.quantum variants with different syntax. Would crosslink to the
    docs but would rather link to the offfical docs when they are public.
    
    closes #857
    
    ---------
    
    Co-authored-by: Alec Edgington <54802828+cqc-alec@users.noreply.github.com>

--- guppylang/std/quantum/__init__.py
+++ guppylang/std/quantum/__init__.py
@@ -49,32 +49,106 @@ def maybe_qubit() -> Option[qubit]:
 
 @guppy.hugr_op(quantum_op("H"))
 @no_type_check
-def h(q: qubit) -> None: ...
+def h(q: qubit) -> None:
+    r"""Hadamard gate command
+
+    .. math::
+        \mathrm{H}= \frac{1}{\sqrt{2}}
+          \begin{pmatrix}
+            1 & 1 \\
+            1 & -1
+          \end{pmatrix}
+    """
 
 
 @guppy.hugr_op(quantum_op("CZ"))
 @no_type_check
-def cz(control: qubit, target: qubit) -> None: ...
+def cz(control: qubit, target: qubit) -> None:
+    r"""Controlled-Z gate command.
+
+    cz(control, target)
+
+    Qubit ordering: [control, target]
+
+    .. math::
+        \mathrm{CZ}=
+          \begin{pmatrix}
+            1 & 0 & 0 & 0 \\
+            0 & 1 & 0 & 0 \\
+            0 & 0 & 1 & 0 \\
+            0 & 0 & 0 & -1
+          \end{pmatrix}
+    """
 
 
 @guppy.hugr_op(quantum_op("CY"))
 @no_type_check
-def cy(control: qubit, target: qubit) -> None: ...
+def cy(control: qubit, target: qubit) -> None:
+    r"""Controlled-Y gate command.
+
+    cy(control, target)
+
+    Qubit ordering: [control, target]
+
+    .. math::
+        \mathrm{CY}=
+          \begin{pmatrix}
+            1 & 0 & 0 & 0 \\
+            0 & 1 & 0 & 0 \\
+            0 & 0 & 0 & -i \\
+            0 & 0 & i & 0
+          \end{pmatrix}
+    """
 
 
 @guppy.hugr_op(quantum_op("CX"))
 @no_type_check
-def cx(control: qubit, target: qubit) -> None: ...
+def cx(control: qubit, target: qubit) -> None:
+    r"""Controlled-X gate command.
+
+    cx(control, target)
+
+    Qubit ordering: [control, target]
+
+    .. math::
+        \mathrm{CX}=
+          \begin{pmatrix}
+            1 & 0 & 0 & 0 \\
+            0 & 1 & 0 & 0 \\
+            0 & 0 & 0 & 1 \\
+            0 & 0 & 1 & 0
+          \end{pmatrix}
+    """
 
 
 @guppy.hugr_op(quantum_op("T"))
 @no_type_check
-def t(q: qubit) -> None: ...
+def t(q: qubit) -> None:
+    r"""T gate.
+
+    .. math::
+        \mathrm{T}=
+          \begin{pmatrix}
+            1 & 0 \\
+            0 & e^{i \frac{\pi}{4}}
+           \end{pmatrix}
+
+    """
 
 
 @guppy.hugr_op(quantum_op("S"))
 @no_type_check
-def s(q: qubit) -> None: ...
+def s(q: qubit) -> None:
+    r"""S gate.
+
+    .. math::
+        \mathrm{S}=
+          \begin{pmatrix}
+            1 & 0 \\
+            0 & i
+           \end{pmatrix}
+
+    """
 
 
 @guppy.hugr_op(quantum_op("V"))
@@ -84,27 +158,77 @@ def v(q: qubit) -> None: ...
 
 @guppy.hugr_op(quantum_op("X"))
 @no_type_check
-def x(q: qubit) -> None: ...
+def x(q: qubit) -> None:
+    r"""X gate.
+
+    .. math::
+        \mathrm{X}=
+          \begin{pmatrix}
+            0 & 1 \\
+            1 & 0
+           \end{pmatrix}
+
+    """
 
 
 @guppy.hugr_op(quantum_op("Y"))
 @no_type_check
-def y(q: qubit) -> None: ...
+def y(q: qubit) -> None:
+    r"""Y gate.
+
+    .. math::
+        \mathrm{Y}=
+          \begin{pmatrix}
+            0 & -i \\
+            i & 0
+           \end{pmatrix}
+
+    """
 
 
 @guppy.hugr_op(quantum_op("Z"))
 @no_type_check
-def z(q: qubit) -> None: ...
+def z(q: qubit) -> None:
+    r"""Z gate.
+
+    .. math::
+        \mathrm{Z}=
+          \begin{pmatrix}
+            1 & 0 \\
+            0 & -1
+           \end{pmatrix}
+
+    """
 
 
 @guppy.hugr_op(quantum_op("Tdg"))
 @no_type_check
-def tdg(q: qubit) -> None: ...
+def tdg(q: qubit) -> None:
+    r"""Tdg gate.
+
+    .. math::
+        \mathrm{T}^\dagger=
+          \begin{pmatrix}
+            1 & 0 \\
+            0 & e^{-i \frac{\pi}{4}}
+           \end{pmatrix}
+
+    """
 
 
 @guppy.hugr_op(quantum_op("Sdg"))
 @no_type_check
-def sdg(q: qubit) -> None: ...
+def sdg(q: qubit) -> None:
+    r"""Sdg gate.
+
+    .. math::
+        \mathrm{S}^\dagger=
+          \begin{pmatrix}
+            1 & 0 \\
+            0 & -i
+           \end{pmatrix}
+
+    """
 
 
 @guppy.hugr_op(quantum_op("Vdg"))
@@ -114,47 +238,116 @@ def vdg(q: qubit) -> None: ...
 
 @guppy.custom(RotationCompiler("Rz"))
 @no_type_check
-def rz(q: qubit, angle: angle) -> None: ...
+def rz(q: qubit, angle: angle) -> None:
+    r"""Rz gate.
+
+    .. math::
+        \mathrm{Rz}(\theta)=
+        \exp(\frac{- i  \theta}{2} Z)=
+          \begin{pmatrix}
+            e^{-\frac{1}{2}i  \theta} & 0 \\
+            0 & e^{\frac{1}{2}i  \theta}
+           \end{pmatrix}
+
+    """
 
 
 @guppy.custom(RotationCompiler("Rx"))
 @no_type_check
-def rx(q: qubit, angle: angle) -> None: ...
+def rx(q: qubit, angle: angle) -> None:
+    r"""Rx gate.
+
+    .. math::
+        \mathrm{Rx}(\theta)=
+          \begin{pmatrix}
+            \cos(\frac{ \theta}{2}) & -i\sin(\frac{ \theta}{2}) \\
+            -i\sin(\frac{ \theta}{2}) & \cos(\frac{ \theta}{2})
+           \end{pmatrix}
+
+    """
 
 
 @guppy.custom(RotationCompiler("Ry"))
 @no_type_check
-def ry(q: qubit, angle: angle) -> None: ...
+def ry(q: qubit, angle: angle) -> None:
+    r"""Ry gate.
+
+    .. math::
+        \mathrm{Ry}(\theta)=
+          \begin{pmatrix}
+            \cos(\frac{\theta}{2}) & -\sin(\frac{ \theta}{2}) \\
+            \sin(\frac{ \theta}{2}) & \cos(\frac{ \theta}{2})
+           \end{pmatrix}
+
+    """
 
 
 @guppy.custom(RotationCompiler("CRz"))
 @no_type_check
-def crz(control: qubit, target: qubit, angle: angle) -> None: ...
+def crz(control: qubit, target: qubit, angle: angle) -> None:
+    r"""Controlled-Rz gate command.
+
+    crz(control, target, theta)
+
+    Qubit ordering: [control, target]
+
+    .. math::
+        \mathrm{CRz}(\theta)=
+          \begin{pmatrix}
+            1 & 0 & 0 & 0 \\
+            0 & 1 & 0 & 0 \\
+            0 & 0 & e^{-\frac{1}{2}i  \theta} & 0 \\
+            0 & 0 & 0 & e^{\frac{1}{2}i  \theta}
+        \end{pmatrix}
+    """
 
 
 @guppy.hugr_op(quantum_op("Toffoli"))
 @no_type_check
-def toffoli(control1: qubit, control2: qubit, target: qubit) -> None: ...
+def toffoli(control1: qubit, control2: qubit, target: qubit) -> None:
+    r"""A Toffoli gate command. Also sometimes known as a CCX gate.
+
+    toffoli(control1, control2, target)
+
+    Qubit ordering: [control1, control2 target]
+
+    .. math::
+        \mathrm{Toffoli}=
+          \begin{pmatrix}
+            1 & 0 & 0 & 0 & 0 & 0 & 0 & 0 \\
+            0 & 1 & 0 & 0 & 0 & 0 & 0 & 0 \\
+            0 & 0 & 1 & 0 & 0 & 0 & 0 & 0 \\
+            0 & 0 & 0 & 1 & 0 & 0 & 0 & 0 \\
+            0 & 0 & 0 & 0 & 1 & 0 & 0 & 0 \\
+            0 & 0 & 0 & 0 & 0 & 1 & 0 & 0 \\
+            0 & 0 & 0 & 0 & 0 & 0 & 0 & 1 \\
+            0 & 0 & 0 & 0 & 0 & 0 & 1 & 0
+          \end{pmatrix}
+    """
 
 
 @guppy.custom(InoutMeasureCompiler())
 @no_type_check
-def project_z(q: qubit) -> bool: ...
+def project_z(q: qubit) -> bool:
+    """Project a single qubit into the Z-basis (a non-destructive measurement)."""
 
 
 @guppy.hugr_op(quantum_op("QFree"))
 @no_type_check
-def discard(q: qubit @ owned) -> None: ...
+def discard(q: qubit @ owned) -> None:
+    """Discard a single qubit."""
 
 
 @guppy.hugr_op(quantum_op("MeasureFree"))
 @no_type_check
-def measure(q: qubit @ owned) -> bool: ...
+def measure(q: qubit @ owned) -> bool:
+    """Measure a single qubit destructively."""
 
 
 @guppy.hugr_op(quantum_op("Reset"))
 @no_type_check
-def reset(q: qubit) -> None: ...
+def reset(q: qubit) -> None:
+    """Reset a single qubit to the |0> state."""
 
 
 N = guppy.nat_var("N")
@@ -181,6 +374,21 @@ def discard_array(qubits: array[qubit, N] @ owned) -> None:
 @guppy
 @no_type_check
 def ch(control: qubit, target: qubit) -> None:
+    r"""Controlled-H gate command.
+
+    ch(control, target)
+
+    Qubit ordering: [control, target]
+
+    .. math::
+        \mathrm{CH} = \frac{1}{\sqrt{2}}
+          \begin{pmatrix}
+            1 & 0 & 0 & 0 \\
+            0 & 1 & 0 & 0 \\
+            0 & 0 & 1 & 1 \\
+            0 & 0 & 1 & -1
+        \end{pmatrix}
+    """
     # based on https://quantumcomputing.stackexchange.com/a/15737
     ry(target, -pi / 4)
     cz(control, target)
