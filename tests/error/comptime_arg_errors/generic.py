384dd8cdebdd68c13279e096e7d74bcb50f9667c
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2025-07-30 15:21:51 +0100

    feat: Extend comptime arguments to arbitrary non-linear types (#1110)
    Closes #1008
    
    Depends on #1033

--- tests/error/comptime_arg_errors/generic.py
+++ tests/error/comptime_arg_errors/generic.py
@@ -1,12 +1,17 @@
 from guppylang import guppy
 from guppylang.std.builtins import comptime
 
-T = guppy.type_var("T")
+T = guppy.type_var("T", copyable=True, droppable=True)
 
 
 @guppy
-def main(q: T @comptime) -> None:
-    pass
+def foo(q: T @comptime) -> T:
+    return T
+
+
+@guppy
+def main() -> int:
+    return foo(42)
 
 
 guppy.compile(main)
