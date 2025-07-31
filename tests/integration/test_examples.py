7a9f7ec55a7b9c2ac40777a2e497b869b7fb543a
Henry Semenenko <henry.semenenko@quantinuum.com>
2025-07-31 10:26:17 +0100

    Add tests for examples

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -1,5 +1,21 @@
 """Tests validating the files in the `examples` directory."""
 
+import pytest
+from pathlib import Path
+
+notebook_files = list(
+    (Path(__file__).parent.parent.parent / "examples").glob("*.ipynb")
+)
+
+
+@pytest.mark.parametrize("notebook", notebook_files)
+def test_example_notebooks(nb_regression, notebook: Path):
+    nb_regression.diff_ignore += (
+        "/metadata/language_info/version",
+        "/cells/*/outputs/*/data/image/png",
+    )
+    nb_regression.check(notebook)
+
 
 def test_demo_notebook(nb_regression):
     nb_regression.diff_ignore += ("/metadata/language_info/version",)
