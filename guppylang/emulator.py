ab9096c8cbe5a32bd5a131ab7272e490863f1d09
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2025-07-31 15:45:19 +0100

    feat: builder pattern run config

--- guppylang/emulator.py
+++ guppylang/emulator.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Any, Protocol
+from dataclasses import dataclass, field, replace
+from typing import TYPE_CHECKING
 
 import selene_sim
 from hugr.qsystem.result import QsysResult
 from selene_core.simulator import Simulator
 from selene_sim.backends.bundled_error_models import IdealErrorModel
-from selene_sim.backends.bundled_simulators import Quest
+from selene_sim.backends.bundled_runtimes import SimpleRuntime
+from selene_sim.backends.bundled_simulators import Coinflip, Quest, Stim
 from selene_sim.event_hooks import EventHook, NoEventHook
+from typing_extensions import Self
 
 if TYPE_CHECKING:
     import datetime
@@ -19,12 +21,13 @@ if TYPE_CHECKING:
     from hugr.qsystem.result import TaggedResult
     from selene_core import BuildPlanner, QuantumInterface, Utility
     from selene_core.error_model import ErrorModel
+    from selene_core.runtime import Runtime
     from selene_sim.instance import SeleneInstance
 
 
 __all__ = [
     "EmulatorInstance",
-    "EmulatorOptions",
+    "EmulatorOpts",
     "EmulatorResult",
     "Simulator",
     "selene_sim",
@@ -36,127 +39,207 @@ class EmulatorResult:
     _qsys_result: QsysResult
 
 
-class Config(Protocol):
-    def update(self, **kwargs: Any) -> None:
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-            else:
-                raise ValueError(f"Invalid configuration key: {key}")
+@dataclass(frozen=True)
+class EmulatorOpts:
+    _n_shots: int = 1
+    _simulator: Simulator = field(default_factory=Coinflip)
+    _runtime: Runtime = field(default_factory=SimpleRuntime)
+    _error_model: ErrorModel = field(default_factory=IdealErrorModel)
+    _event_hook: EventHook = field(default_factory=NoEventHook)
+    _verbose: bool = False
+    _timeout: datetime.timedelta | None = None
+    _results_logfile: Path | None = None
+    _random_seed: int | None = None
+    _shot_offset: int = 0
+    _shot_increment: int = 1
+    _n_processes: int = 1
 
+    @property
+    def n_shots(self) -> int:
+        return self._n_shots
 
-@dataclass
-class EmulatorOptions(Config):
-    error_model: ErrorModel = field(default_factory=IdealErrorModel)
-    event_hook: EventHook = field(default_factory=NoEventHook)
-    verbose: bool = False
-    timeout: datetime.timedelta | None = None
-    results_logfile: Path | None = None
-    random_seed: int | None = None
-    shot_offset: int = 0
-    shot_increment: int = 1
-    n_processes: int = 1
+    @property
+    def simulator(self) -> Simulator:
+        return self._simulator
+
+    @property
+    def runtime(self) -> Runtime:
+        return self._runtime
+
+    @property
+    def error_model(self) -> ErrorModel:
+        return self._error_model
+
+    @property
+    def event_hook(self) -> EventHook:
+        return self._event_hook
+
+    @property
+    def verbose(self) -> bool:
+        return self._verbose
+
+    @property
+    def timeout(self) -> datetime.timedelta | None:
+        return self._timeout
+
+    @property
+    def results_logfile(self) -> Path | None:
+        return self._results_logfile
+
+    @property
+    def random_seed(self) -> int | None:
+        return self._random_seed
+
+    @property
+    def shot_offset(self) -> int:
+        return self._shot_offset
+
+    @property
+    def shot_increment(self) -> int:
+        return self._shot_increment
+
+    @property
+    def n_processes(self) -> int:
+        return self._n_processes
+
+    def with_n_shots(self, value: int) -> Self:
+        return replace(self, _n_shots=value)
+
+    def with_simulator(self, value: Simulator) -> Self:
+        return replace(self, _simulator=value)
+
+    def with_runtime(self, value: Runtime) -> Self:
+        return replace(self, _runtime=value)
+
+    def with_error_model(self, value: ErrorModel) -> Self:
+        return replace(self, _error_model=value)
+
+    def with_event_hook(self, value: EventHook) -> Self:
+        return replace(self, _event_hook=value)
+
+    def with_verbose(self, value: bool) -> Self:
+        return replace(self, _verbose=value)
+
+    def with_timeout(self, value: datetime.timedelta | None) -> Self:
+        return replace(self, _timeout=value)
+
+    def with_results_logfile(self, value: Path | None) -> Self:
+        return replace(self, _results_logfile=value)
+
+    def with_random_seed(self, value: int | None) -> Self:
+        out = replace(self, _random_seed=value)
+        # TODO flaky stateful, remove when selene simplifies
+        out.simulator.random_seed = value
+        return out
+
+    def with_shot_offset(self, value: int) -> Self:
+        return replace(self, _shot_offset=value)
+
+    def with_shot_increment(self, value: int) -> Self:
+        return replace(self, _shot_increment=value)
+
+    def with_n_processes(self, value: int) -> Self:
+        return replace(self, _n_processes=value)
+
+    @classmethod
+    def statevector(cls) -> Self:
+        return cls().with_simulator(Quest())
+
+    @classmethod
+    def coinflip(cls) -> Self:
+        return cls().with_simulator(Coinflip())
+
+    @classmethod
+    def stabilizer(cls) -> Self:
+        return cls().with_simulator(Stim())
 
 
 @dataclass
 class EmulatorInstance:
     _instance: SeleneInstance
-    options: EmulatorOptions = field(default_factory=EmulatorOptions)
 
-    def run(
-        self, simulator: Simulator, n_qubits: int, n_shots: int = 1
-    ) -> EmulatorResult:
-        result_stream = self._run_instance(
-            simulator=simulator, n_qubits=n_qubits, n_shots=n_shots
-        )
+    def run(self, n_qubits: int, options: EmulatorOpts | None = None) -> EmulatorResult:
+        result_stream = self._run_instance(n_qubits=n_qubits, options=options)
 
         # TODO progress bar on consuming iterator?
 
         qsys_res = QsysResult(result_stream)
         return EmulatorResult(_qsys_result=qsys_res)
 
-    def run_statevector(self, n_qubits: int, n_shots: int = 1) -> EmulatorResult:
-        """Run with the default statevector simulation backend."""
-        return self.run(
-            simulator=Quest(self.options.random_seed),
-            n_qubits=n_qubits,
-            n_shots=n_shots,
-        )
-
-    # def run_stabilizer(self, n_shots: int = 1) -> EmulatorResult:
-    #     """Run with the default stabilizer simulation backend."""
-    #     return self.run(simulator=Stim(self.options.random_seed), n_shots=n_shots)
-
-    # def run_coinflip(self, n_shots: int = 1) -> EmulatorResult:
-    #     """Run with the default coinflip simulation backend."""
-    #     return self.run(
-    #         simulator=Coinflip(random_seed=self.options.random_seed), n_shots=n_shots
-    #     )
-
-    # def run_classical_replay(
-    #     self, measurements: Sequence[Sequence[bool]], n_shots: int = 1
-    # ) -> EmulatorResult:
-    #     """Run with the default classical replay simulation backend."""
-    #     return self.run(
-    #         simulator=ClassicalReplay(
-    #             measurements=list(map(list, measurements)),
-    #             random_seed=self.options.random_seed,
-    #         ),
-    #         n_shots=n_shots,
-    # )
-
     def _run_instance(
-        self, simulator: Simulator, n_qubits: int, n_shots: int
+        self, n_qubits: int, options: EmulatorOpts | None = None
     ) -> Iterator[Iterator[TaggedResult]]:
         """Run the Selene instance with the given simulator."""
+        options = options or EmulatorOpts()
+
         return self._instance.run_shots(
-            simulator=simulator,
+            simulator=options.simulator,
             n_qubits=n_qubits,
-            n_shots=n_shots,
-            event_hook=self.options.event_hook,
-            error_model=self.options.error_model,
-            verbose=self.options.verbose,
-            timeout=self.options.timeout,
-            results_logfile=self.options.results_logfile,
-            random_seed=self.options.random_seed,
-            shot_offset=self.options.shot_offset,
-            shot_increment=self.options.shot_increment,
-            n_processes=self.options.n_processes,
+            n_shots=options.n_shots,
+            event_hook=options.event_hook,
+            error_model=options.error_model,
+            verbose=options.verbose,
+            timeout=options.timeout,
+            results_logfile=options.results_logfile,
+            random_seed=options.random_seed,
+            shot_offset=options.shot_offset,
+            shot_increment=options.shot_increment,
+            n_processes=options.n_processes,
         )
 
-    def set_seed(self, seed: int) -> None:
-        """Set the random seed for the emulator instance."""
-        self.options.random_seed = seed
 
-
-@dataclass
-class EmulatorBuilder(Config):
+@dataclass(frozen=True)
+class EmulatorBuilder:
     """A builder class for creating EmulatorInstance objects."""
 
-    name: str | None = None
-    build_dir: Path | None = None
-    interface: QuantumInterface | None = None
-    utilities: Sequence[Utility] | None = None
-    verbose: bool = False
-    planner: BuildPlanner | None = None
-    progress_bar: bool = False
-    strict: bool = False
-    save_planner: bool = False
+    # interface supported parameters
+    _name: str | None = None
+    _build_dir: Path | None = None
+    _verbose: bool = False
+
+    # selene_sim supported parameters, may be added in the future
+    _planner: BuildPlanner | None = None
+    _utilities: Sequence[Utility] | None = None
+    _interface: QuantumInterface | None = None
+    _progress_bar: bool = False
+    _strict: bool = False
+    _save_planner: bool = False
+
+    @property
+    def name(self) -> str | None:
+        return self._name
+
+    @property
+    def build_dir(self) -> Path | None:
+        return self._build_dir
+
+    @property
+    def verbose(self) -> bool:
+        return self._verbose
+
+    def with_name(self, value: str | None) -> Self:
+        return replace(self, _name=value)
+
+    def with_build_dir(self, value: Path | None) -> Self:
+        return replace(self, _build_dir=value)
+
+    def with_verbose(self, value: bool) -> Self:
+        return replace(self, _verbose=value)
 
     def build(self, package: Package) -> EmulatorInstance:
         """Build an EmulatorInstance from a compiled package."""
 
         instance = selene_sim.build(  # type: ignore[attr-defined]
             package,
-            name=self.name,
-            build_dir=self.build_dir,
-            interface=self.interface,
-            utilities=self.utilities,
-            verbose=self.verbose,
-            planner=self.planner,
-            progress_bar=self.progress_bar,
-            strict=self.strict,
-            save_planner=self.save_planner,
+            name=self._name,
+            build_dir=self._build_dir,
+            interface=self._interface,
+            utilities=self._utilities,
+            verbose=self._verbose,
+            planner=self._planner,
+            progress_bar=self._progress_bar,
+            strict=self._strict,
+            save_planner=self._save_planner,
         )
 
         return EmulatorInstance(_instance=instance)
--- tests/integration/emulator/test_simple.py
+++ tests/integration/emulator/test_simple.py
@@ -1,7 +1,7 @@
 from guppylang.decorator import guppy
 from guppylang.std.builtins import result
 from guppylang.std.quantum import qubit, measure, h
-from guppylang.emulator import QsysResult
+from guppylang.emulator import QsysResult, EmulatorOpts
 
 
 def test_basic_emulation() -> None:
@@ -10,7 +10,7 @@ def test_basic_emulation() -> None:
         result("c", measure(qubit()))
 
     em = main.build_emulator()
-    res = em.run_statevector(n_qubits=1)
+    res = em.run(1, EmulatorOpts.statevector())
     expected = QsysResult([[("c", False)]])
     assert res._qsys_result == expected
 
@@ -21,7 +21,6 @@ def test_basic_emulation() -> None:
         result("c", measure(q))
 
     em = main.build_emulator()
-    em.set_seed(42)
-    res = em.run_statevector(n_qubits=1)
+    res = em.run(1, EmulatorOpts.statevector().with_random_seed(42))
     expected = QsysResult([[("c", True)]])
     assert res._qsys_result == expected
