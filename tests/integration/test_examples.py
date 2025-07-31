84fdadd6b04ed0549e98e1e3e124f6036e593dab
Henry Semenenko <henry.semenenko@quantinuum.com>
2025-07-31 16:51:26 +0100

    Replace example with notebook

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -36,9 +36,3 @@ def test_random_walk_qpe(validate):
     from examples.random_walk_qpe import hugr
 
     validate(hugr)
-
-
-def test_t_factory(validate):
-    from examples.t_factory import hugr
-
-    validate(hugr)
