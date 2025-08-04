665caa8989365e9b55ce117174566aea1cd101e7
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2025-08-04 14:27:25 +0100

    delete unecessary overload

--- guppylang/defs.py
+++ guppylang/defs.py
@@ -5,19 +5,15 @@ with the compiler-internal definition objects in the `definitions` module.
 """
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, ClassVar, Generic, ParamSpec, TypeVar, cast
+from typing import Any, ClassVar, Generic, ParamSpec, TypeVar, cast
 
 from hugr.package import Package
 
-from guppylang.definition.common import MonomorphizableDef
 from guppylang.engine import ENGINE
 from guppylang.tracing.object import TracingDefMixin
 from guppylang.tracing.util import hide_trace
 
 from .emulator import EmulatorBuilder, EmulatorInstance
-
-if TYPE_CHECKING:
-    from guppylang.compiler.core import PartiallyMonomorphizedArgs
 
 P = ParamSpec("P")
 Out = TypeVar("Out")
@@ -45,20 +41,6 @@ class GuppyFunctionDefinition(GuppyDefinition, Generic[P, Out]):
     @hide_trace
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> Out:
         return cast(Out, super().__call__(*args, **kwargs))
-
-    def compile(self) -> Package:
-        """Compile a Guppy function definition to HUGR."""
-        compiled_module = ENGINE.compile(self.id)
-
-        # Look up how many generic params the function has so we can create an empty
-        # partial monomorphization to look up in the context
-        checked_def = ENGINE.checked[self.id]
-        mono_args: PartiallyMonomorphizedArgs | None = None
-        if isinstance(checked_def, MonomorphizableDef):
-            mono_args = tuple(None for _ in checked_def.params)
-
-        _ = ENGINE.compiled[self.id, mono_args]
-        return compiled_module.package
 
     def emulator(
         self, n_qubits: int, builder: EmulatorBuilder | None = None
