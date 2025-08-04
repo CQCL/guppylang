231c68dcef1ad417416b2e62393a0be03b241fc0
Mark Koch <mark.koch@quantinuum.com>
2025-08-04 18:09:06 +0100

    Move dummy module to guppylang

--- guppylang/__init__.py
+++ guppylang/__init__.py
@@ -1,7 +1,7 @@
 from guppylang_internals.experimental import enable_experimental_features
-from guppylang_internals.module import GuppyModule
 
 from guppylang.decorator import guppy
+from guppylang.module import GuppyModule
 from guppylang.std import builtins, debug, quantum
 from guppylang.std.builtins import array, comptime, py
 from guppylang.std.quantum import qubit
--- guppylang/module.py
+++ guppylang/module.py
@@ -1,3 +1,13 @@
-from guppylang_internals.module import GuppyModule
+from typing import Any
 
-__all__ = ("GuppyModule",)
+
+class GuppyModule:
+    """A Guppy module that may contain function and type definitions."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        err = (
+            "Explicit Guppy modules are no longer supported. Use the regular `@guppy` "
+            "decorator without passing a module and use `foo.compile()` to "
+            "compile Guppy functions."
+        )
+        raise RuntimeError(err)
