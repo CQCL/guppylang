afed85fc333f75566963359425e3dea369dc306b
Agustín Borgna <agustin.borgna@quantinuum.com>
2025-08-05 12:19:12 +0100

    tmp skip test

--- tests/integration/test_none.py
+++ tests/integration/test_none.py
@@ -1,5 +1,9 @@
+import pytest
+
 from guppylang.decorator import guppy
 
+
+@pytest.mark.skip(reason="TODO: Testing release-checks. Unskip this")
 def test_none(validate, run_int_fn):
     @guppy
     def foo() -> None:
