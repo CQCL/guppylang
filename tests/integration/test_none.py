5796cfc9eab9d9f1b2f0f2250470aed2c32779a5
Agustín Borgna <agustin.borgna@quantinuum.com>
2025-08-05 12:23:55 +0100

    Revert "tmp skip test"
    This reverts commit afed85fc333f75566963359425e3dea369dc306b.

--- tests/integration/test_none.py
+++ tests/integration/test_none.py
@@ -1,9 +1,5 @@
-import pytest
-
 from guppylang.decorator import guppy
 
-
-@pytest.mark.skip(reason="TODO: Testing release-checks. Unskip this")
 def test_none(validate, run_int_fn):
     @guppy
     def foo() -> None:
