7f606c778d98312a0d1c4a9c7a27448c24d80585
Craig Roy <croyzor@users.noreply.github.com>
2025-08-05 12:03:03 +0100

    fix: Support `None` value (#1149)
    Resolves #1148

---
+++ tests/integration/test_none.py
@@ -0,0 +1,17 @@
+from guppylang.decorator import guppy
+
+def test_none(validate, run_int_fn):
+    @guppy
+    def foo() -> None:
+        return None
+
+    @guppy
+    def bar(x: None, y: int) -> int:
+        return y
+
+    @guppy
+    def main() -> int:
+        return bar(foo(), 42)
+
+    validate(main.compile())
+    run_int_fn(main, expected=42)
