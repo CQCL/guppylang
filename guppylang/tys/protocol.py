1c6a88a4b19b7e5c163ff046c446f26b58ff674e
Mark Koch <mark.koch@quantinuum.com>
2025-08-06 12:09:23 +0100

    wip ImplProofs and proof from assumption

--- guppylang/tys/protocol.py
+++ guppylang/tys/protocol.py
@@ -4,9 +4,14 @@ from typing import Sequence
 
 from guppylang.definition.common import DefId
 from guppylang.tys.arg import Argument
+from guppylang.tys.subst import Subst
 
 
 @dataclass(frozen=True)
 class ProtocolInst():
     type_args: Sequence[Argument]
     definition: DefId
+
+    def substitute(self, subst: Subst) -> "ProtocolInst":
+        # TODO
+        pass
