9408120f023743838ac404a3082d195645732718
Mark Koch <mark.koch@quantinuum.com>
2025-08-07 15:01:04 +0100

    Make test work for Python < 3.12

--- tests/error/errors_on_usage/shadow_param.py
+++ tests/error/errors_on_usage/shadow_param.py
@@ -1,9 +1,11 @@
 from guppylang import guppy
 from guppylang.std.builtins import nat
 
+n = guppy.nat_var("n")
+
 
 @guppy
-def main[n: nat](b: bool) -> nat:
+def main(b: bool) -> nat:
     if b:
         n = nat(42)
     return n
