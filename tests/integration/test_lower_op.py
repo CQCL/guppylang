02fdcba7e1a9c0f9b50fb5db4acb1fbbf163f596
Henry Semenenko <henry.semenenko@quantinuum.com>
2025-08-18 14:36:14 +0100

    Test multiple operations in single program. Add test that op is added to hugr extension

--- tests/integration/test_lower_op.py
+++ tests/integration/test_lower_op.py
@@ -19,12 +19,29 @@ def test_auto_hugr_lowering(validate):
     def main() -> None:
         q0 = qubit()
         q1 = qubit()
+        q2 = qubit()
         entangle(q0, q1)
+        entangle(q1, q2)
         measure(q0)
         measure(q1)
+        measure(q2)
 
     hugr = main.compile()
 
     hugr.extensions.append(test_hugr_ext)
 
     validate(hugr)
+
+
+def test_lower_funcs_hugr(validate):
+    test_hugr_ext = he.Extension("test_hugr_ext", SemanticVersion(0, 1, 0))
+
+    @lowerable_op(test_hugr_ext)
+    def entangle(q0: qubit, q1: qubit) -> None:
+        h(q0)
+        cx(q0, q1)
+
+    op = test_hugr_ext.get_op("entangle")
+
+    for funcs in op.lower_funcs:
+        validate(funcs.hugr)
