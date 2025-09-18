f9f52cf487c0b5459a5b246a54aac1dd5d4ed1bf
hkengo-qtnm <kengo.hirata@quantinuum.com>
2025-09-18 16:09:07 +0100

    modifier_compiler

--- guppylang-internals/src/guppylang_internals/compiler/func_compiler.py
+++ guppylang-internals/src/guppylang_internals/compiler/func_compiler.py
@@ -3,12 +3,14 @@ from typing import TYPE_CHECKING
 from guppylang_internals.ast_util import get_type
 from guppylang_internals.checker.func_checker import linear_front_others_back
 from guppylang_internals.checker.stmt_checker import qubit_ty
-from guppylang_internals.compiler.expr_compiler import ExprCompiler
+from guppylang_internals.compiler.expr_compiler import ExprCompiler, array_unwrap_elem, array_wrap_elem
+from guppylang_internals.std._internal.compiler.array import array_convert_from_std_array, array_convert_to_std_array, array_map, array_new, standard_array_type, unpack_array
 from guppylang_internals.std._internal.compiler.tmp_modifier_exts import MODIFIER_EXTENSION
-from guppylang_internals.tys.builtin import array_type, is_array_type
+from guppylang_internals.tys.builtin import array_type, int_type, is_array_type
 from guppylang_internals.tys.ty import InputFlags
 from hugr import Wire, ops
 from hugr import tys as ht
+import hugr
 from hugr.build.function import Function
 
 from guppylang_internals.compiler.cfg_compiler import compile_cfg
@@ -167,64 +169,108 @@ def compile_modifier(
                       dagger_ty, [in_out_arg, other_in_arg]),
             call,
         )
-    if modifier.is_power():
-        # TODO (k.hirata): hugr signature is wrong. It does not take an integer argument.
-        power_ty = ht.FunctionType([hugr_ty], [hugr_ty])
-        for _power in modifier.power:
-            call = dfg.builder.add_op(
-                ops.ExtOp(MODIFIER_EXTENSION.get_op(POWER_OP_NAME),
-                          power_ty, [in_out_arg, other_in_arg]),
-                call,
-            )
+    qubit_num_args = []
     if modifier.has_control():
         for control in modifier.control:
-            # control extension op
+            # definition of types
             assert control.qubit_num is not None
             if isinstance(control.qubit_num, int):
                 qubit_num = ht.BoundedNatArg(control.qubit_num)
             else:
                 qubit_num = control.qubit_num.to_arg().to_hugr(ctx)
-            control_ty = array_type(qubit_ty(), control.qubit_num)
-            ctrl_hugr_ty = control_ty.to_hugr(ctx)
+            qubit_num_args.append(qubit_num)
+            std_array = standard_array_type(ht.Qubit, qubit_num)
 
+            # control operator
             input_fn_ty = hugr_ty
             output_fn_ty = ht.FunctionType(
-                [ctrl_hugr_ty, *hugr_ty.input], [ctrl_hugr_ty, *hugr_ty.output]
+                [std_array, *hugr_ty.input], [std_array, *hugr_ty.output]
             )
-
-            # TODO: HELP ME!: need to convert value_array to array in hugr
             op = MODIFIER_EXTENSION.get_op(CONTROL_OP_NAME).instantiate(
                 [qubit_num, in_out_arg, other_in_arg],
                 ht.FunctionType([input_fn_ty], [output_fn_ty])
             )
             call = dfg.builder.add_op(op, call)
-
-            # update the type of wire
+            # update types
+            in_out_arg = ht.ListArg([std_array.type_arg(), *in_out_arg.elems])
             hugr_ty = output_fn_ty
+    if modifier.is_power():
+        # TODO (k.hirata): hugr signature is wrong. It does not take an integer argument.
+        power_ty = ht.FunctionType([hugr_ty, int_type().to_hugr(ctx)], [hugr_ty])
+        for power in modifier.power:
+            num = expr_compiler.compile(power.iter, dfg)
+            call = dfg.builder.add_op(
+                ops.ExtOp(MODIFIER_EXTENSION.get_op(POWER_OP_NAME),
+                          power_ty, [in_out_arg, other_in_arg]),
+                call, num
+            )
 
-            # argument of control
-            ty = get_type(control.ctrl[0])
-            if is_array_type(ty):
-                c = expr_compiler.visit(control.ctrl[0])
-                args.insert(0, c)
-            else:
-                TODO
-                # apply_array_op_with_conversions(
-                #     self.ctx, self.builder, op, ht.Qubit, num_qubits_arg, qubits_in[0]
-                # )
+    # Prepare control arguments
+    ctrl_args = []
+    for i, control in enumerate(modifier.control):
+        if is_array_type(get_type(control.ctrl[0])):
+            input_array = expr_compiler.compile(control.ctrl[0], dfg)
 
+            unwrap = array_unwrap_elem(ctx)
+            unwrap = dfg.builder.load_function(
+                unwrap,
+                instantiation=ht.FunctionType([ht.Option(ht.Qubit)], [ht.Qubit]),
+                type_args=[ht.TypeTypeArg(ht.Qubit)]
+            )
+            map_op = array_map(ht.Option(ht.Qubit), qubit_num_args[i], ht.Qubit)
+            unwrapped_array = dfg.builder.add_op(map_op, input_array, unwrap)
+
+            unwrapped_array = dfg.builder.add_op(
+                array_convert_to_std_array(ht.Qubit, qubit_num_args[i]), unwrapped_array
+            )
+
+            ctrl_args.extend(unwrapped_array)
+        else:
+            cs = [expr_compiler.compile(c, dfg) for c in control.ctrl]
+            c = dfg.builder.add_op(
+                array_new(ht.Qubit, len(control.ctrl)), *cs
+            )
+            val_to_std = array_convert_to_std_array(ht.Qubit, qubit_num_args[i])
+            c = dfg.builder.add_op(val_to_std, *c)
+            ctrl_args.append(c)
+
+    # Call
     call = dfg.builder.add_op(
         ops.CallIndirect(),
         call,
-        *args,
+        *ctrl_args, *args,
     )
     wires = iter(call)
-    for control in modifier.control:
-        for c in control.ctrl:
-            if not isinstance(c, PlaceNode):
-                next(wires)
-            else:
-                dfg[c.place] = next(wires)
+
+    # Unpack controls
+    for i, control in enumerate(modifier.control):
+        wire = next(wires)
+        if is_array_type(get_type(control.ctrl[0])):
+            result_array = dfg.builder.add_op(
+                array_convert_from_std_array(ht.Qubit, qubit_num_args[i]),  wire
+            )
+
+            wrap = array_wrap_elem(ctx)
+            wrap = dfg.builder.load_function(
+                wrap,
+                instantiation=ht.FunctionType([ht.Qubit], [ht.Option(ht.Qubit)]),
+                type_args=[ht.TypeTypeArg(ht.Qubit)]
+            )
+            map_op = array_map(ht.Qubit, qubit_num_args[i], ht.Option(ht.Qubit))
+            wire = dfg.builder.add_op(map_op, result_array, wrap)
+
+            c = control.ctrl[0]
+            assert isinstance(c, PlaceNode)
+
+            dfg[c.place] = wire
+        else:
+            val_from_std = array_convert_from_std_array(ht.Qubit, qubit_num_args[i])
+            wire = dfg.builder.add_op(val_from_std, wire)
+            unpacked = unpack_array(dfg.builder, wire)
+            for c, wire in zip(control.ctrl, unpacked):
+                assert isinstance(c, PlaceNode)
+                dfg[c.place] = wire
+
     for arg in captured:
         if InputFlags.Inout in arg.flags:
             dfg[arg] = next(wires)
