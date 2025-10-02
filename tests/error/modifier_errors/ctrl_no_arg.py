97197e58aa44edff02136244acd1cd9d84b3eae6
hkengo-qtnm <kengo.hirata@quantinuum.com>
2025-10-02 13:47:33 +0100

    changed a TODO comment to a normal comment

--- tests/error/modifier_errors/ctrl_no_arg.py
+++ tests/error/modifier_errors/ctrl_no_arg.py
@@ -1,7 +1,7 @@
 from guppylang.decorator import guppy
 
 
-# TODO: `control` is overloaded, so the number of arguments need not be fixed to 1.
+# `control` is overloaded, so the number of arguments need not be fixed to 1.
 # However, the error message says "expected 1 arguments, got 0" when no argument is given.
 @guppy
 def test() -> None:
