54d99beccef74f172303c357e6704e8acb74946e
Mark Koch <mark.koch@quantinuum.com>
2025-11-19 16:11:51 +0000

    fix!: Allow comptime string arguments as result tags

--- guppylang-internals/src/guppylang_internals/std/_internal/debug.py
+++ guppylang-internals/src/guppylang_internals/std/_internal/debug.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from typing import ClassVar, cast
 
 from guppylang_internals.ast_util import with_loc
+from guppylang_internals.checker.core import ComptimeVariable
 from guppylang_internals.checker.errors.generic import ExpectedError
 from guppylang_internals.checker.errors.type_errors import WrongNumberOfArgsError
 from guppylang_internals.checker.expr_checker import (
@@ -14,14 +15,14 @@ from guppylang_internals.definition.custom import CustomCallChecker
 from guppylang_internals.definition.ty import TypeDef
 from guppylang_internals.diagnostic import Error
 from guppylang_internals.error import GuppyTypeError
-from guppylang_internals.nodes import StateResultExpr
-from guppylang_internals.std._internal.checker import TAG_MAX_LEN, TooLongError
+from guppylang_internals.nodes import GenericParamValue, PlaceNode, StateResultExpr
 from guppylang_internals.tys.builtin import (
     get_array_length,
     get_element_type,
     is_array_type,
     string_type,
 )
+from guppylang_internals.tys.const import Const, ConstValue
 from guppylang_internals.tys.ty import (
     FuncInput,
     FunctionType,
@@ -43,12 +44,16 @@ class StateResultChecker(CustomCallChecker):
 
     def synthesize(self, args: list[ast.expr]) -> tuple[ast.expr, Type]:
         tag, _ = ExprChecker(self.ctx).check(args[0], string_type())
-        if not isinstance(tag, ast.Constant) or not isinstance(tag.value, str):
-            raise GuppyTypeError(ExpectedError(tag, "a string literal"))
-        if len(tag.value.encode("utf-8")) > TAG_MAX_LEN:
-            err: Error = TooLongError(tag)
-            err.add_sub_diagnostic(TooLongError.Hint(None))
-            raise GuppyTypeError(err)
+        tag_value: Const
+        match tag:
+            case ast.Constant(value=str(v)):
+                tag_value = ConstValue(string_type(), v)
+            case PlaceNode(place=ComptimeVariable(static_value=str(v))):
+                tag_value = ConstValue(string_type(), v)
+            case GenericParamValue() as param_value:
+                tag_value = param_value.param.to_bound().const
+            case _:
+                raise GuppyTypeError(ExpectedError(tag, "a string literal"))
         syn_args: list[ast.expr] = [tag]
 
         if len(args) < 2:
@@ -90,6 +95,10 @@ class StateResultChecker(CustomCallChecker):
         args, ret_ty, inst = synthesize_call(func_ty, syn_args, self.node, self.ctx)
         assert len(inst) == 0, "func_ty is not generic"
         node = StateResultExpr(
-            tag=tag.value, args=args, func_ty=func_ty, array_len=array_len
+            tag_value=tag_value,
+            tag_expr=tag,
+            args=args,
+            func_ty=func_ty,
+            array_len=array_len,
         )
         return with_loc(self.node, node), ret_ty
